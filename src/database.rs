e8c2223a2e60d315e3e31fcd2ae2e232ff720439
Billy Kirk <kirk.william.c@gmail.com>
2021-04-10 14:51:35 -0500

    Optimizations using if let

--- src/database.rs
+++ src/database.rs
@@ -1,11 +1,11 @@
-use serde_json;
+use serde_json::{Map, Value};
 use std::collections::HashMap;
 use std::fs::write;
 use std::vec::Vec;
 
 pub struct Database {
     config: DatabaseConfig,
-    documents: Vec<serde_json::Value>,
+    documents: Vec<Value>,
 }
 
 pub struct DatabaseConfig {
@@ -20,7 +20,7 @@ impl Database {
         }
     }
 
-    pub fn insert_one(&mut self, document: serde_json::Value) -> Result<(), String> {
+    pub fn insert_one(&mut self, document: Value) -> Result<(), String> {
         match document.as_object() {
             None => return Err("documents to insert were invalid".to_string()),
             Some(_) => self.documents.push(document),
@@ -29,7 +29,7 @@ impl Database {
         self.save()
     }
 
-    pub fn insert_many(&mut self, documents: serde_json::Value) -> Result<(), String> {
+    pub fn insert_many(&mut self, documents: Value) -> Result<(), String> {
         match documents.as_array() {
             None => return Err("documents to insert were invalid".to_string()),
             Some(docs) => self.documents.append(&mut docs.to_owned()),
@@ -38,18 +38,22 @@ impl Database {
         self.save()
     }
 
-    pub fn find_one(&self, query: serde_json::Value) -> Option<serde_json::Value> {
+    pub fn find_one(&self, query: Value) -> Option<Value> {
+        let query = query.as_object()?;
+
         match self.search_documents(query) {
             None => None,
             Some(found) => Some(self.documents[found[0]].to_owned()),
         }
     }
 
-    pub fn find_many(&self, query: serde_json::Value) -> Option<Vec<serde_json::Value>> {
+    pub fn find_many(&self, query: Value) -> Option<Vec<Value>> {
+        let query = query.as_object()?;
+
         match self.search_documents(query) {
             None => None,
             Some(found) => {
-                let mut results: Vec<serde_json::Value> = Vec::new();
+                let mut results: Vec<Value> = Vec::new();
 
                 for index in found {
                     results.push(self.documents[index].to_owned())
@@ -60,19 +64,15 @@ impl Database {
         }
     }
 
-    pub fn update_one(
-        &mut self,
-        query: serde_json::Value,
-        update: serde_json::Value,
-    ) -> Result<(), String> {
-        match query.as_object() {
+    pub fn update_one(&mut self, query: Value, update: Value) -> Result<(), String> {
+        let query = match query.as_object() {
             None => return Err("query was invalid".to_string()),
-            Some(_) => (),
+            Some(query) => query,
         };
 
-        let updates_object = match update.as_object() {
+        let update = match update.as_object() {
             None => return Err("updates were invalid".to_string()),
-            Some(updates) => updates,
+            Some(update) => update,
         };
 
         let found = match self.search_documents(query) {
@@ -84,29 +84,25 @@ impl Database {
             None => return Err("document to delete not found".to_string()),
             Some(doc) => {
                 let mut temp = doc.to_owned();
-                for (key, value) in updates_object.iter() {
+                for (key, value) in update.iter() {
                     temp.insert(key.to_owned(), value.to_owned());
                 }
-                self.documents[found[0]] = serde_json::Value::Object(temp);
+                self.documents[found[0]] = Value::Object(temp);
             }
         }
 
         self.save()
     }
 
-    pub fn update_many(
-        &mut self,
-        query: serde_json::Value,
-        update: serde_json::Value,
-    ) -> Result<(), String> {
-        match query.as_object() {
+    pub fn update_many(&mut self, query: Value, update: Value) -> Result<(), String> {
+        let query = match query.as_object() {
             None => return Err("query was invalid".to_string()),
-            Some(_) => (),
+            Some(query) => query,
         };
 
-        let updates_object = match update.as_object() {
+        let update = match update.as_object() {
             None => return Err("updates were invalid".to_string()),
-            Some(updates) => updates,
+            Some(update) => update,
         };
 
         let found_map = match self.search_documents(query) {
@@ -125,10 +121,10 @@ impl Database {
                 None => return Err("document to delete not found".to_string()),
                 Some(doc) => {
                     let mut temp = doc.to_owned();
-                    for (key, value) in updates_object.iter() {
+                    for (key, value) in update.iter() {
                         temp.insert(key.to_owned(), value.to_owned());
                     }
-                    self.documents[index] = serde_json::Value::Object(temp);
+                    self.documents[index] = Value::Object(temp);
                 }
             }
         }
@@ -136,10 +132,10 @@ impl Database {
         self.save()
     }
 
-    pub fn delete_one(&mut self, query: serde_json::Value) -> Result<(), String> {
-        match query.as_object() {
+    pub fn delete_one(&mut self, query: Value) -> Result<(), String> {
+        let query = match query.as_object() {
             None => return Err("query was invalid".to_string()),
-            Some(_) => (),
+            Some(query) => query,
         };
 
         match self.search_documents(query) {
@@ -152,10 +148,10 @@ impl Database {
         self.save()
     }
 
-    pub fn delete_many(&mut self, query: serde_json::Value) -> Result<(), String> {
-        match query.as_object() {
+    pub fn delete_many(&mut self, query: Value) -> Result<(), String> {
+        let query = match query.as_object() {
             None => return Err("query was invalid".to_string()),
-            Some(_) => (),
+            Some(query) => query,
         };
 
         match self.search_documents(query) {
@@ -178,28 +174,23 @@ impl Database {
         self.save()
     }
 
-    fn search_documents(&self, query: serde_json::Value) -> Option<Vec<usize>> {
+    fn search_documents(&self, query: &Map<String, Value>) -> Option<Vec<usize>> {
         let mut found: Vec<usize> = Vec::new();
 
         for (index, document) in self.documents.iter().enumerate() {
             let mut include = true;
 
-            match query.as_object() {
-                None => break,
-                Some(pairs) => {
-                    for (key, query_value) in pairs {
-                        if !include {
-                            break;
-                        }
+            for (query_key, query_value) in query {
+                if !include {
+                    break;
+                }
 
-                        let document_value = &document[key];
+                let document_value = &document[query_key];
 
-                        include = self.match_values(query_value, document_value);
+                include = self.match_values(query_value, document_value);
 
-                        if include {
-                            found.push(index);
-                        }
-                    }
+                if include {
+                    found.push(index);
                 }
             }
         }
@@ -211,11 +202,7 @@ impl Database {
         Some(found)
     }
 
-    fn match_values(
-        &self,
-        query_value: &serde_json::Value,
-        document_value: &serde_json::Value,
-    ) -> bool {
+    fn match_values(&self, query_value: &Value, document_value: &Value) -> bool {
         if query_value == document_value {
             return true;
         }
